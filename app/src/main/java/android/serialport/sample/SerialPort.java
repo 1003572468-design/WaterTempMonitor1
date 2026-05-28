2392a8ccb592d79f5854c628d3629df53dc51c57
1003572468-design <1003572468@qq.com>
2026-05-28 21:30:30 +0800

    Update SerialPort.java

--- app/src/main/java/android/serialport/sample/SerialPort.java
+++ app/src/main/java/android/serialport/sample/SerialPort.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.security.InvalidParameterException;   // 添加这一行
 
 public class SerialPort {
     private static final String TAG = "SerialPort";
