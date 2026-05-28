56068d8f945b73dd92682fbff83d260f0d62732d
1003572468-design <1003572468@qq.com>
2026-05-28 17:36:20 +0800

    Create SerialPort.java

---
+++ app/src/main/java/android/serialport/sample/SerialPort.java
@@ -0,0 +1,53 @@
+package android.serialport.sample;
+
+import android.util.Log;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class SerialPort {
+    private static final String TAG = "SerialPort";
+    private FileDescriptor mFd;
+    private FileInputStream mFileInputStream;
+    private FileOutputStream mFileOutputStream;
+    
+    public SerialPort(File device, int baudrate, int flags) throws SecurityException, IOException, InvalidParameterException {
+        if (!device.canRead() || !device.canWrite()) {
+            try {
+                Process su = Runtime.getRuntime().exec("/system/bin/su");
+                String cmd = "chmod 666 " + device.getAbsolutePath() + "\n exit\n";
+                su.getOutputStream().write(cmd.getBytes());
+                su.waitFor();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new SecurityException();
+            }
+        }
+        
+        mFd = open(device.getAbsolutePath(), baudrate, flags);
+        if (mFd == null) {
+            Log.e(TAG, "native open returns null");
+            throw new IOException();
+        }
+        mFileInputStream = new FileInputStream(mFd);
+        mFileOutputStream = new FileOutputStream(mFd);
+    }
+    
+    private native static FileDescriptor open(String path, int baudrate, int flags);
+    public native void close();
+    
+    public FileInputStream getInputStream() {
+        return mFileInputStream;
+    }
+    
+    public FileOutputStream getOutputStream() {
+        return mFileOutputStream;
+    }
+    
+    static {
+        System.loadLibrary("serial_port");
+    }
+}
