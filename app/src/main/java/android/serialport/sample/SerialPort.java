d254c8b67032e431d7f8d8c7e93a4b5e2c34bb43
1003572468-design <1003572468@qq.com>
2026-05-29 21:05:12 +0800

    Update SerialPort.java

--- app/src/main/java/android/serialport/sample/SerialPort.java
+++ app/src/main/java/android/serialport/sample/SerialPort.java
@@ -7,28 +7,38 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.security.InvalidParameterException;
 
 public class SerialPort {
     private static final String TAG = "SerialPort";
     private FileInputStream mFileInputStream;
     private FileOutputStream mFileOutputStream;
     
-    public SerialPort(File device, int baudrate, int flags) throws SecurityException, IOException, InvalidParameterException {
-        if (!device.canRead() || !device.canWrite()) {
-            try {
-                Process su = Runtime.getRuntime().exec("/system/bin/su");
-                String cmd = "chmod 666 " + device.getAbsolutePath() + "\n exit\n";
-                su.getOutputStream().write(cmd.getBytes());
-                su.waitFor();
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new SecurityException();
-            }
-        }
-        
+    public SerialPort(File device, int baudrate, int flags) throws Exception {
+        grantRootPermission(device);
         mFileInputStream = new FileInputStream(device);
         mFileOutputStream = new FileOutputStream(device);
+        Log.d(TAG, "串口打开成功: " + device.getAbsolutePath());
+    }
+    
+    private void grantRootPermission(File device) {
+        String[] suPaths = {"/system/bin/su", "/system/xbin/su", "/su/bin/su", "/sbin/su"};
+        
+        for (String suPath : suPaths) {
+            File suFile = new File(suPath);
+            if (suFile.exists()) {
+                try {
+                    Log.d(TAG, "使用 su: " + suPath);
+                    Process process = Runtime.getRuntime().exec(suPath);
+                    String cmd = "chmod 666 " + device.getAbsolutePath() + "\n exit\n";
+                    process.getOutputStream().write(cmd.getBytes());
+                    process.waitFor();
+                    Log.d(TAG, "权限修改成功");
+                    break;
+                } catch (Exception e) {
+                    Log.e(TAG, "su 失败: " + suPath, e);
+                }
+            }
+        }
     }
     
     public InputStream getInputStream() {
@@ -41,12 +51,8 @@ public class SerialPort {
     
     public void close() {
         try {
-            if (mFileInputStream != null) {
-                mFileInputStream.close();
-            }
-            if (mFileOutputStream != null) {
-                mFileOutputStream.close();
-            }
+            if (mFileInputStream != null) mFileInputStream.close();
+            if (mFileOutputStream != null) mFileOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
